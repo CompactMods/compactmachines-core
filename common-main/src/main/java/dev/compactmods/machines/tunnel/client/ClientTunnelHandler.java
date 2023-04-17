30240cc9d2b1094758333808540ae355f7b2bb6d
Ted Senft <code@robotgryphon.com>
2023-04-17 04:16:34 -0400

    Add more tunnel def handling to ClientTunnelHandler/ITunnelHolder

--- common-main/src/main/java/dev/compactmods/machines/tunnel/client/ClientTunnelHandler.java
+++ common-main/src/main/java/dev/compactmods/machines/tunnel/client/ClientTunnelHandler.java
@@ -4,9 +4,27 @@ import dev.compactmods.machines.api.tunnels.ITunnelHolder;
 import dev.compactmods.machines.api.tunnels.TunnelDefinition;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
 
 public class ClientTunnelHandler {
+
+    @Deprecated(forRemoval = true, since = "2.2.1")
     public static void setTunnel(BlockPos position, TunnelDefinition type) {
+        var level = Minecraft.getInstance().level;
+        if(level == null) return;
+
+        Minecraft.getInstance().tell(() -> {
+            if (level.getBlockEntity(position) instanceof ITunnelHolder tun) {
+                try {
+                    tun.setTunnelType(type);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static void setTunnel(BlockPos position, ResourceKey<TunnelDefinition> type) {
         var level = Minecraft.getInstance().level;
         if(level == null) return;
 
--- tunnels-api/src/main/java/dev/compactmods/machines/api/tunnels/ITunnelHolder.java
+++ tunnels-api/src/main/java/dev/compactmods/machines/api/tunnels/ITunnelHolder.java
@@ -1,8 +1,13 @@
 package dev.compactmods.machines.api.tunnels;
 
+import net.minecraft.resources.ResourceKey;
+
 public interface ITunnelHolder {
 
     TunnelDefinition getTunnelType();
 
+    @Deprecated(forRemoval = true, since = "2.2.1")
     void setTunnelType(TunnelDefinition type);
+
+    void setTunnelType(ResourceKey<TunnelDefinition> type);
 }
