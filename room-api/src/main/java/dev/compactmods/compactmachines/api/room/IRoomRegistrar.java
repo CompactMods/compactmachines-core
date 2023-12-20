247db6ecadd5372fcf0e15fdb0cf2795395f7bcb
Ted Senft <code@robotgryphon.com>
2023-12-20 05:41:51 -0500

    Shorthand/registration methods for room api + registrar

--- room-api/src/main/java/dev/compactmods/compactmachines/api/room/IRoomRegistrar.java
+++ room-api/src/main/java/dev/compactmods/compactmachines/api/room/IRoomRegistrar.java
@@ -3,10 +3,21 @@ package dev.compactmods.compactmachines.api.room;
 import dev.compactmods.compactmachines.api.room.registration.IRoomBuilder;
 
 import java.util.Optional;
+import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 public interface IRoomRegistrar {
+
+    default RoomInstance createNew(RoomTemplate template, UUID owner) {
+        return createNew(template, owner, override -> override);
+    }
+
+    default RoomInstance createNew(RoomTemplate template, UUID owner, Function<IRoomBuilder, IRoomBuilder> override) {
+        return createNew(builder -> builder.defaultMachineColor(template.color())
+                .owner(owner)
+                .dimensions(template.dimensions()));
+    }
 
     RoomInstance createNew(Function<IRoomBuilder, IRoomBuilder> build);
 
--- room-api/src/main/java/dev/compactmods/compactmachines/api/room/RoomApi.java
+++ room-api/src/main/java/dev/compactmods/compactmachines/api/room/RoomApi.java
@@ -9,12 +9,14 @@ import net.minecraft.server.MinecraftServer;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public class RoomApi {
 
     /**
      * Set up when the server or single-player instance changes.
      * NOT for API consumers to use! Use the methods provided here for safety.
+     *
      * @since 3.0.0
      */
     @ApiStatus.Internal
@@ -22,11 +24,22 @@ public class RoomApi {
 
     /**
      * Fetches a room instance from the registrar.
+     *
      * @param roomCode The room identifier.
      * @since 3.0.0
      */
     public static Optional<RoomInstance> room(String roomCode) {
         return INSTANCE.registrar().get(roomCode);
+    }
+
+    /**
+     * Registers a new room instance.
+     * @param template
+     * @param owner
+     * @return
+     */
+    public static RoomInstance newRoom(RoomTemplate template, UUID owner) {
+        return INSTANCE.registrar().createNew(template, owner);
     }
 
     public static IRoomRegistrar registrar() {
